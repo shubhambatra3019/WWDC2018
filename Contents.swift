e995fbba121edfa116a346d40f0a94d3fb930108
shubhambatra3019 <31996668+shubhambatra3019@users.noreply.github.com>
2018-03-24 16:33:57 -0400

    Added Moving Balls

--- Contents.swift
+++ Contents.swift
@@ -5,56 +5,35 @@ import SpriteKit
 
 class GameScene: SKScene {
     
-    private var label : SKLabelNode!
-    private var spinnyNode : SKShapeNode!
+    var numberBall = SKSpriteNode()
+    var numberBall2 = SKSpriteNode()
     
     override func didMove(to view: SKView) {
-        // Get label node from scene and store it for use later
-        label = childNode(withName: "//helloLabel") as? SKLabelNode
-        label.alpha = 0.0
-        let fadeInOut = SKAction.sequence([.fadeIn(withDuration: 2.0),
-                                           .fadeOut(withDuration: 2.0)])
-        label.run(.repeatForever(fadeInOut))
         
-        // Create shape node to use during mouse interaction
-        let w = (size.width + size.height) * 0.05
-        
-        spinnyNode = SKShapeNode(rectOf: CGSize(width: w, height: w), cornerRadius: w * 0.3)
-        spinnyNode.lineWidth = 2.5
-        
-        let fadeAndRemove = SKAction.sequence([.wait(forDuration: 0.5),
-                                               .fadeOut(withDuration: 0.5),
-                                               .removeFromParent()])
-        spinnyNode.run(.repeatForever(.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-        spinnyNode.run(fadeAndRemove)
+        numberBall = self.childNode(withName: "numberBall") as! SKSpriteNode
+        numberBall2 = self.childNode(withName: "numberBall2") as! SKSpriteNode
+        numberBall2.physicsBody?.applyImpulse(CGVector(dx: -30, dy: -30))
+        numberBall.physicsBody?.applyImpulse(CGVector(dx: 20, dy: 20 ))
+        let border = SKPhysicsBody(edgeLoopFrom: self.frame)
+        border.friction = 0
+        border.restitution = 1
+        self.physicsBody = border
     }
     
     func touchDown(atPoint pos : CGPoint) {
-        guard let n = spinnyNode.copy() as? SKShapeNode else { return }
         
-        n.position = pos
-        n.strokeColor = SKColor.green
-        addChild(n)
     }
     
     func touchMoved(toPoint pos : CGPoint) {
-        guard let n = self.spinnyNode.copy() as? SKShapeNode else { return }
         
-        n.position = pos
-        n.strokeColor = SKColor.blue
-        addChild(n)
     }
     
     func touchUp(atPoint pos : CGPoint) {
-        guard let n = spinnyNode.copy() as? SKShapeNode else { return }
-        
-        n.position = pos
-        n.strokeColor = SKColor.red
-        addChild(n)
+        print("Hello")
     }
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { touchMoved(toPoint: t.location(in: self)) }
+        
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -62,7 +41,7 @@ class GameScene: SKScene {
     }
     
     override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { touchUp(atPoint: t.location(in: self)) }
+        
     }
     
     override func update(_ currentTime: TimeInterval) {
@@ -71,7 +50,7 @@ class GameScene: SKScene {
 }
 
 // Load the SKScene from 'GameScene.sks'
-let sceneView = SKView(frame: CGRect(x:0 , y:0, width: 640, height: 480))
+let sceneView = SKView(frame: CGRect(x:0 , y:0, width: 300, height: 600))
 if let scene = GameScene(fileNamed: "GameScene") {
     // Set the scale mode to scale to fit the window
     scene.scaleMode = .aspectFill
